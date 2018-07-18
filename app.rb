b388ca96f38477149d5d0acf3435f7010dd50a28
anjalisebastian <anjalisebastian@icloud.com>
2018-07-18 15:39:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,5 +20,7 @@ class App < Sinatra::Base
     @num1 = params[:num1].to_i 
     @num2 = params[:num2].to_i 
     " Product is #{@num1 * @num2}"
+    
+    # Product is #{params[:num1]} * #{params[:num2]}
 
 end
